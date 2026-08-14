e982095526c142c2bb3f078d540003a16ed56cea
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2026-08-14 09:51:41 +0200

    https://github.com/CRLG/GROSBOT_STM32/issues/2 Correction du problème de message Lidar corrompu (perte d'octets reçus)
    Voir explication dans le ticket

--- CM7/Core/Src/stm32h7xx_hal_msp.c
+++ CM7/Core/Src/stm32h7xx_hal_msp.c
@@ -1071,7 +1071,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     __HAL_LINKDMA(huart,hdmatx,hdma_usart3_tx);
 
     /* USART3 interrupt Init */
-    HAL_NVIC_SetPriority(USART3_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(USART3_IRQn, 6, 0);
     HAL_NVIC_EnableIRQ(USART3_IRQn);
   /* USER CODE BEGIN USART3_MspInit 1 */
 
@@ -1127,7 +1127,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     __HAL_LINKDMA(huart,hdmarx,hdma_usart6_rx);
 
     /* USART6 interrupt Init */
-    HAL_NVIC_SetPriority(USART6_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(USART6_IRQn, 5, 0);
     HAL_NVIC_EnableIRQ(USART6_IRQn);
   /* USER CODE BEGIN USART6_MspInit 1 */
 
